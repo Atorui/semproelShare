2ebd5afa2f6984b04bfcdc8753ab438092a66162
Atorui <fakhrudine@gmail.com>
2016-07-18 07:23:32 +0700

    Update semproelShare.js

--- src/semproelShare.js
+++ src/semproelShare.js
@@ -13,106 +13,124 @@
        platformPreciseCount = {},
        totalPreciseCount, paltformAttributeIndicator, hostElementForCountInfo,
        
-       fetchJsonp = (function(root, doc, htmlBody) {
-          function createInjectedScript(url, callbackName) {
-             var injectedScript = doc.createElement('script');
-             
-             injectedScript.async = true;
-             injectedScript.src = url + callbackName;
-             
-             return injectedScript;
-          }
-          
-          return function(url) {
-             return new Promise(function(resolve, reject) {
-                var callbackName = 'kolbek' + Math.round(100000 * Math.random()),
-                    injectedScript = createInjectedScript(url, callbackName);
-               
-                root[callbackName] = function(data) {
-                   resolve(data);
-                };
-                   
-                injectedScript.onerror = function() {
-                   reject("Network error! Either you lost connection to the server before the data is received or error was occuring in the origin server of " + url);
-                   // Here also need to be cleaned up
-                   root[callbackName] = null;
-                   delete root[callbackName];
-                   htmlBody.removeChild(injectedScript);
-                   injectedScript = null;
-                };
-                
-                injectedScript.onload = function() {
-                   root[callbackName] = null;
-                   delete root[callbackName];
-                   htmlBody.removeChild(injectedScript);
-                   injectedScript = null;
-                };
-                
-                htmlBody.appendChild(injectedScript);
-             });
-          };
-       }(WIN, DOC, DOC.body)),
+       abbreWithPoint = function(s, i, n, g, o) {
+          this[1].innerHTML = this[0].slice(s, i) + "." + this[0].slice(n, g) + o;
+       },
        
-       executeClickShare = (function(root, doc) {
-          // Credits: http://www.xtf.dk/2011/08/center-new-popup-window-even-on.html with trivial modification
-          // param {url, title, popupWidth, popupHeight}
-          function popCenteredWindow(param) {
-             var dualScreenLeft = root.screenX || root.screenLeft,
-                 dualScreenTop = root.screenY || root.screenTop,
-                 currentBrowserWidth = root.innerWidth || doc.documentElement.clientWidth || doc.body.clientWidth,
-                 currentBrowserHeight = root.innerHeight || doc.documentElement.clientHeight || doc.body.clientHeight,
-                 left = ((currentBrowserWidth / 2) - (param.popupWidth / 2)) + dualScreenLeft,
-                 top = ((currentBrowserHeight / 2) - (param.popupHeight / 2)) + dualScreenTop;
-                
-             var popWindow = root.open(param.url, param.title, "width=" + param.popupWidth + ",height=" + param.popupHeight + ",top=" + top + ",left=" + left + ",titlebar=yes,toolbar=no,menubar=no,location=yes,resizable=no,scrollbars=yes,status=yes");
-            
-             if (root.focus) {
-                popWindow.focus();
-             }
-            
-             return popWindow;
-          }
+       abbreNoPoint = function(a, s, u) {
+          this[1].innerHTML = this[0].slice(a, s) + u;
+       },
+       
+       asIs = function(nulledValue) {
+          this[1].innerHTML = this[0];
+       },
+       
+       countAppearanceDeterminer = {
+          4: { functn: abbreWithPoint, argmnt: [0, 1, 1, 2, "k"] },
+          5: { functn: abbreWithPoint, argmnt: [0, 2, 2, 3, "k"] },
+          6: { functn: abbreNoPoint, argmnt: [0, 3, "k"] },
+          7: { functn: abbreWithPoint, argmnt: [0, 1, 1, 2, "m"] },
+          8: { functn: abbreWithPoint, argmnt: [0, 2, 2, 3, "m"] },
+          9: { functn: abbreNoPoint, argmnt: [0, 3, "m"] },
+          10: { functn: abbreWithPoint, argmnt: [0, 1, 1, 2, "g"] },
+          deflt: { functn: asIs, argmnt: [null] }
+       };
+       
+   function createInjectedScriptForJsonp(url, callbackName) {
+      var injectedScript = DOC.createElement('script');
+      
+      injectedScript.async = true;
+      injectedScript.src = url + callbackName;
+      
+      return injectedScript;
+   }
+   
+   function clearInjectedScriptForJsonp(root, callbackName, htmlBody, injectedScript) {
+      root[callbackName] = null;
+      delete root[callbackName];
+      htmlBody.removeChild(injectedScript);
+      injectedScript = null;
+   }
+   
+   function fetchJsonp(url) {
+      return new Promise(function(resolve, reject) {
+         var root = WIN, htmlBody = DOC.body,
+             callbackName = 'kolbek' + Math.round(100000 * Math.random()),
+             injectedScript = createInjectedScriptForJsonp(url, callbackName);
+      
+         root[callbackName] = function(data) {
+            resolve(data);
+         };
           
-          // pK = platformKitchen
-          return function(clickedPlatform, pK) {
-             var popupWindow = popCenteredWindow({
-                                  url: pK[clickedPlatform].urlSharer, // sementara
-                                  title: "Share on " + clickedPlatform,
-                                  popupWidth: 750,
-                                  popupHeight: 450
-                               }),
-                 isPopupWindowClosed,
-                 currentClickedPlatformCount = platformPreciseCount[clickedPlatform];
-                 
-             isPopupWindowClosed = root.setInterval(function() {
-                if (popupWindow.closed === true) {
-                   root.clearInterval(isPopupWindowClosed);
-                   root.setTimeout(function() {
-                      fetchJsonp(pK[clickedPlatform].urlCountGetter).then(pK[clickedPlatform].success.bind(platformPreciseCount)).then(
-                         function(responseCount) {
-                            console.log("Success fetching individual count exec");
-                            totalPreciseCount = (totalPreciseCount - currentClickedPlatformCount) + responseCount;
-                            determineShareCountAppearance(totalPreciseCount.toString(), hostElementForCountInfo);
-                         },
-                         
-                         function(error) {
-                            throw new Error(semproelShareText + "\"Fetching " + clickedPlatform + " count error! Reason: " + error + "\"");
-                         }
-                      );
-                   }, pK[clickedPlatform].wait || 0);
-                }
-             }, 250);
-          };
-       }(WIN, DOC));
+         injectedScript.onerror = function() {
+            reject("Network error! Either you lost connection to the server before the data is received or error was occuring in the origin server of " + url);
+            clearInjectedScriptForJsonp(root, callbackName, htmlBody, injectedScript);
+         };
+       
+         injectedScript.onload = function() {
+            clearInjectedScriptForJsonp(root, callbackName, htmlBody, injectedScript);
+         };
+       
+         htmlBody.appendChild(injectedScript);
+      });
+   }
+   
+   function popCenteredWindow(param, root, doc) {
+      var dualScreenLeft = root.screenX || root.screenLeft,
+          dualScreenTop = root.screenY || root.screenTop,
+          currentBrowserWidth = root.innerWidth || doc.documentElement.clientWidth || doc.body.clientWidth,
+          currentBrowserHeight = root.innerHeight || doc.documentElement.clientHeight || doc.body.clientHeight,
+          left = ((currentBrowserWidth / 2) - (param.popupWidth / 2)) + dualScreenLeft,
+          top = ((currentBrowserHeight / 2) - (param.popupHeight / 2)) + dualScreenTop;
+       
+      var popWindow = root.open(param.url, param.title, "width=" + param.popupWidth + ",height=" + param.popupHeight + ",top=" + top + ",left=" + left + ",titlebar=yes,toolbar=no,menubar=no,location=yes,resizable=no,scrollbars=yes,status=yes");
+      
+      if (root.focus) {
+         popWindow.focus();
+      }
+      
+      return popWindow;
+   }
+   
+   function executeClickShare(clickedPlatform, pK) {
+      var root = WIN, doc = DOC,
+          popupWindow = popCenteredWindow({
+                           url: pK[clickedPlatform].urlSharer,
+                           title: "Share on " + clickedPlatform,
+                           popupWidth: 750,
+                           popupHeight: 450
+                        }, root, doc),
+          isPopupWindowClosed,
+          // Ofcourse, we need to store this in var first, because the value will change when we need this current value
+          currentClickedPlatformCount = platformPreciseCount[clickedPlatform];
+        
+      isPopupWindowClosed = root.setInterval(function() {
+         if (popupWindow.closed === true) {
+            root.clearInterval(isPopupWindowClosed);
+            root.setTimeout(function() {
+               fetchJsonp(pK[clickedPlatform].urlCountGetter).then(pK[clickedPlatform].success.bind(platformPreciseCount)).then(
+                  function(responseCount) {
+                     totalPreciseCount = (totalPreciseCount - currentClickedPlatformCount) + responseCount;
+                     determineShareCountAppearance(totalPreciseCount.toString(), hostElementForCountInfo, countAppearanceDeterminer);
+                  },
+                
+                  function(error) {
+                     throw new Error(semproelShareText + "\"Fetching " + clickedPlatform + " count error! Reason: " + error + "\"");
+                  }
+               );
+            }, pK[clickedPlatform].wait || 0);
+         }
+      }, 250);
+   }
        
    /*
-    | the param just exactly as written below when called
-    |
-    | param: {
-    |    platformKitchen: platformKitchen,
-    |    sharePlatform: param.sharePlatform,
-    |    perPlatformCount: platformPreciseCount
-    | }
+    * the param just exactly as written below when called
+    *
+    * param: {
+    *    platformKitchen: platformKitchen,
+    *    sharePlatform: param.sharePlatform,
+    *    perPlatformCount: platformPreciseCount
+    * }
     */
    function fetchUnifiedCount(param) {
       var promiseOfShareCount = [],
@@ -129,39 +147,11 @@
       return promiseOfShareCount;
    }
 
-   function determineShareCountAppearance(stringifiedTotalCount, hostElementForCount) {
-      switch (stringifiedTotalCount.length) {
-         case 4:
-            hostElementForCount.innerHTML = stringifiedTotalCount.slice(0, 1) + "." + stringifiedTotalCount.slice(1, 2) + "k";
-            break;
-         
-         case 5:
-            hostElementForCount.innerHTML = stringifiedTotalCount.slice(0, 2) + "." + stringifiedTotalCount.slice(2, 3) + "k";
-            break;
-         
-         case 6:
-            hostElementForCount.innerHTML = stringifiedTotalCount.slice(0, 3) + "k";
-            break;
-            
-         case 7:
-            hostElementForCount.innerHTML = stringifiedTotalCount.slice(0, 1) + "." + stringifiedTotalCount.slice(1, 2) + "m";
-            break;
-            
-         case 8:
-            hostElementForCount.innerHTML = stringifiedTotalCount.slice(0, 2) + "." + stringifiedTotalCount.slice(2, 3) + "m";
-            break;
-         
-         case 9:
-            hostElementForCount.innerHTML = stringifiedTotalCount.slice(0, 3) + "m";
-            break;
-            
-         case 10:
-            hostElementForCount.innerHTML = stringifiedTotalCount.slice(0, 1) + "." + stringifiedTotalCount.slice(1, 2) + "g";
-            break;
-         
-         default:
-            hostElementForCount.innerHTML = stringifiedTotalCount;
-      }
+   // appearanceDeterminer = countAppearanceDeterminer
+   function determineShareCountAppearance(stringifiedTotalCount, hostElementForCount, appearanceDeterminer) {
+      var t = appearanceDeterminer[stringifiedTotalCount.length] || appearanceDeterminer.deflt;
+      
+      t.functn.apply(arguments, t.argmnt);
    }
    
    function respondClickShare(e) {
@@ -180,10 +170,6 @@
    }
 
    function semproelShare(param) {
-      if (arguments.length < 1) {
-         throw new Error(semproelShareText + "\"You are missing a parameter. Please check the documentation.\"");
-      }
-      
       var requiredParam = ["share", "shareCount", "sharePlatform", "shareButtonContainer"],
           i = requiredParam.length;
           
@@ -219,7 +205,7 @@
                                     pK[platformList[i]].urlSharer = pK[platformList[i]].urlSharer.replace("{{url}}", currentEncodedUri).replace("{{title}}", root.encodeURIComponent(par.pageTitle)).replace("{{description}}", root.encodeURIComponent(par.pageDescription)).replace("{{media}}", root.encodeURI(par.pageMedia));
                                     pK[platformList[i]].urlCountGetter += currentEncodedUri + "&callback=";
                                  } else {
-                                    throw new Error(semproelShareText + "One of your hook at \"semproelShare.mediaPlatform.yourSocialMediaHook\" is not available among values of \"" + paltformAttributeIndicator + "\". They should not duplicate (only one at hook and only one becomes the value of " + paltformAttributeIndicator+ ") and should correlate equally (i.e. be the same). For instance if at semproelShare.mediaPlatform hook is \"semproelShare.mediaPlatform.facebook\", then at " + paltformAttributeIndicator + " should look like \"" + paltformAttributeIndicator + "='facebook'\".");
+                                    throw new Error(semproelShareText + "One of your hook at \"semproelShare.mediaPlatform.yourSocialMediaHook\" is not available among values of \"" + paltformAttributeIndicator + "\". They should not duplicate (only one at hook and only one becomes the value of " + paltformAttributeIndicator + ") and should correlate equally (i.e. be the same). For instance if at semproelShare.mediaPlatform hook is \"semproelShare.mediaPlatform.facebook\", then at " + paltformAttributeIndicator + " should look like \"" + paltformAttributeIndicator + "='facebook'\". Another case, it may not be a function, while it should be.");
                                  }
                               }
                               
@@ -240,7 +226,7 @@
             totalPreciseCount = response.reduce(function(a, b) {
                return a + b;
             });
-            determineShareCountAppearance(totalPreciseCount.toString(), param.shareCount);
+            determineShareCountAppearance(totalPreciseCount.toString(), param.shareCount, countAppearanceDeterminer);
             WIN.removeEventListener("load", initiateUnifiedCountFetch);
          })["catch"](function(error) {
             WIN.removeEventListener("load", initiateUnifiedCountFetch);
